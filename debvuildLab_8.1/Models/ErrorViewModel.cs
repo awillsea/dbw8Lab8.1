78ff83397f04252f430de709f231ade5d1524ef1
AJ Willsea <AJWillsea@rocketmortgage.com>
2022-09-20 00:15:43 -0400

    first commit

---
+++ debvuildLab_8.1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace debvuildLab_8._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
